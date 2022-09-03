2946aa2659434d6c5de572e5d3b0fc7b5ead8f5a
“Semoud” <semoud.o@gmail.com>
2022-09-03 11:48:58 +0100

    adjusted text style responsiveness

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -8,12 +8,12 @@ const FeaturesCard = ({ data }) => {
 	}
 
 	return (
-		<div className='flex w-full flex-col items-start sm:max-w-[33.3333333333%] sm:flex-row'>
-			<img className=' w-10' src={urlFor(data.icon)} alt={data.title} />
-			<div className='mt-6 sm:mt-0 sm:ml-6'>
-				<h2 className='text-3xl font-semibold'>{data.title}</h2>
-				<p className='mt-6'>{data.body}</p>
-				<a className='mt-6 block' href={data.link} target='_blank' rel='noreferrer'>
+		<div className='flex w-full flex-col items-start sm:flex-row md:flex-col lg:flex-row'>
+			<img className='w-6 md:w-8 lg:w-10' src={urlFor(data.icon)} alt={data.title} />
+			<div className='mt-2 sm:mt-0 sm:ml-4 md:ml-0 md:mt-4 lg:ml-6 lg:mt-0'>
+				<h2 className='text-lg font-semibold md:text-xl lg:text-3xl'>{data.title}</h2>
+				<p className='mt-2 md:mt-4 lg:mt-6'>{data.body}</p>
+				<a className='mt-2 block md:mt-4 lg:mt-6' href={data.link} target='_blank' rel='noreferrer'>
 					{data.cta + ' ›'}
 				</a>
 			</div>
--- src/components/Features.js
+++ src/components/Features.js
@@ -13,7 +13,7 @@ const Features = () => {
 
 	return (
 		<div className='py-7 md:py-14 lg:py-28'>
-			<div className='main-container flex flex-col gap-14 lg:flex-row lg:gap-10'>{feature && feature.map(item => <FeaturesCard data={item} key={item._id} />)}</div>
+			<div className='main-container grid grid-cols-1 grid-rows-none gap-14 md:grid-cols-3 md:gap-8 lg:gap-10'>{feature && feature.map(item => <FeaturesCard data={item} key={item._id} />)}</div>
 		</div>
 	);
 };
