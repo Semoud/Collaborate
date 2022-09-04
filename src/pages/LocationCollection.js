353da43513ba6872b1173b5fd359160b323ff822
“Semoud” <semoud.o@gmail.com>
2022-09-04 14:48:09 +0100

    adjusted no results message styling

--- src/pages/LocationCollection.js
+++ src/pages/LocationCollection.js
@@ -31,7 +31,11 @@ function LocationCollection() {
 		console.log(activeFilters);
 	}, [activeFilters]);
 
-	const [location, setLocation] = useState([]);
+	const resetFilters = () => {
+		setActiveFilters([]);
+	};
+
+	const [location, setLocation] = useState();
 	useEffect(() => {
 		sanityClient
 			.fetch(`*[_type == "location"]{_id, slug, title, location, gallery}`)
@@ -45,7 +49,6 @@ function LocationCollection() {
 				<div className='grid-col-1 relative grid gap-3 md:grid-cols-2'>
 					<div>
 						<h2 className='text-3xl font-semibold md:text-4xl'>All buildings</h2>
-						<p>Found {location.length} locations</p>
 					</div>
 					{amenities && (
 						<div className='overflow-hidden'>
@@ -59,7 +62,42 @@ function LocationCollection() {
 						</div>
 					)}
 				</div>
-				<div className='mt-5 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:mt-10 md:grid-cols-3 lg:mt-20 xl:gap-12'>{location && location.map(item => <LocationsCard data={item} scroll={false} key={item._id} />)}</div>
+				{location ? (
+					<>
+						{location.length >= 1 ? (
+							<div className='mt-5 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:mt-10 md:grid-cols-3 lg:mt-20 xl:gap-12'>
+								{location.map(item => (
+									<LocationsCard data={item} scroll={false} key={item._id} />
+								))}
+							</div>
+						) : (
+							<div className='mx-auto mt-16 flex flex-col items-center justify-center gap-2 sm:flex-row sm:items-start'>
+								<img src={'./assets/telescope-light.svg'} alt='No results found' className='w-10 sm:mt-1 sm:w-8' />
+								<div className='mt-3 flex flex-col items-center text-center sm:ml-3 sm:mt-0 sm:items-start sm:text-left'>
+									<p className='text-lg font-medium '>Sorry, we couldn't find the location you was looking for.</p>
+									<p>We wasn’t able to find any matches, please try modifying your search criteria.</p>
+									<button onClick={resetFilters} className='mt-3 flex gap-2'>
+										<img src={'./assets/arrow-rotate-right-regular.svg'} alt='No results found' className='w-4' />
+										Reset filters
+									</button>
+								</div>
+							</div>
+						)}
+					</>
+				) : (
+					// FIXME: ADD SKELETON
+					<p>LOADING</p>
+				)}
+				{/* <div className='mt-5 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:mt-10 md:grid-cols-3 lg:mt-20 xl:gap-12'>
+					{location.length >= 1 ? (
+						location.map(item => <LocationsCard data={item} scroll={false} key={item._id} />)
+					) : (
+						<div className='flex gap-2'>
+							<img src={'./assets/telescope-light.svg'} alt='No results found' className='w-2 md:w-3' />
+							<p className='text-lg font-semibold md:text-xl lg:text-3xl'>Sorry, we couldn't find the location you was looking for.</p>
+						</div>
+					)}
+				</div> */}
 			</div>
 		</div>
 	);
