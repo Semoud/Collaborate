2f6b55ef056a9d356af99f6a6c10d2ff1b6b271e
“Semoud” <semoud.o@gmail.com>
2022-09-11 18:13:30 +0100

    added mobile responsive styling

--- client/src/components/FormError.js
+++ client/src/components/FormError.js
@@ -2,8 +2,8 @@ import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
 
 const FormError = ({ title, message }) => {
 	return (
-		<div className='mt-4 flex border border-red-500 bg-red-100 py-3 px-5'>
-			<ExclamationCircleIcon class='mr-4 mt-1 h-6 w-6' />
+		<div className='mt-4 border border-red-500 bg-red-100 py-3 px-5 sm:flex'>
+			<ExclamationCircleIcon class='mb-1 h-6 w-6 sm:mr-4 sm:mt-1' />
 			<div>
 				<p className='text-lg font-semibold'>{title ? title : 'Looks like something went wrong!'}</p>
 				<p>{message ? message : 'Please check your input and try again.'}</p>
--- client/src/components/FormSuccess.js
+++ client/src/components/FormSuccess.js
@@ -2,8 +2,8 @@ import { CheckCircleIcon } from '@heroicons/react/24/outline';
 
 const FormSuccess = ({ title, message }) => {
 	return (
-		<div className='mt-4 flex border border-slate-500 bg-slate-100 py-3 px-5'>
-			<CheckCircleIcon class='mr-4 mt-1 h-6 w-6' />
+		<div className='mt-4 border border-slate-500 bg-slate-100 py-3 px-5 sm:flex'>
+			<CheckCircleIcon class='mb-1 h-6 w-6 sm:mr-4 sm:mt-1' />
 			<div>
 				<p className='text-lg font-semibold'>{title ? title : 'Thanks for getting in contact!'}</p>
 				<p>{message ? message : 'A sales representative from our team will be in touch soon.'}</p>
