6070d8a68b57256a68c292afacff114ce18fb82d
“Semoud” <semoud.o@gmail.com>
2022-09-25 21:58:01 +0100

    pulling data from body

--- client/functions/submission/submission.js
+++ client/functions/submission/submission.js
@@ -3,14 +3,16 @@ Mailer.setApiKey(process.env.SG_API_KEY);
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async event => {
+	const body = JSON.parse(event.body);
+
 	const message = {
 		to: process.env.SG_MAILER_TO,
 		from: process.env.SG_MAILER_FROM,
 		subject: 'You have received a new lead!',
 		text: 'this is where the text is',
-		html: `<p><strong>You got a new lead!</strong></p><br/><p><strong>Full name:</strong> ${event.queryStringParameters.name}</p><p><strong>E-mail address:</strong> ${event.queryStringParameters.email}</p><p><strong>Telephone number:</strong> ${event.queryStringParameters.telephone}</p><p><strong>Country:</strong> ${event.queryStringParameters.country}</p><p><strong>Comapny name:</strong> ${event.queryStringParameters.company}</p><p><strong>Team size:</strong> ${event.queryStringParameters.teamSize}</p>`,
+		html: `<p><strong>You got a new lead!</strong></p><br/><p><strong>Full name:</strong> ${body.name}</p><p><strong>E-mail address:</strong> ${body.email}</p><p><strong>Telephone number:</strong> ${body.telephone}</p><p><strong>Country:</strong> ${body.country}</p><p><strong>Comapny name:</strong> ${body.company}</p><p><strong>Team size:</strong> ${body.teamSize}</p>`,
 	};
-	const jsonBody = JSON.parse(event.body);
+
 	try {
 		Mailer.send(message);
 		return {
