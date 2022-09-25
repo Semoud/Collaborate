6a4b353de05e98896ccf74d5d568082d555dc2a2
“Semoud” <semoud.o@gmail.com>
2022-09-25 21:33:14 +0100

    added sendgrid to server function

--- client/functions/submission/submission.js
+++ client/functions/submission/submission.js
@@ -3,17 +3,28 @@ Mailer.setApiKey(process.env.SG_API_KEY);
 
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async event => {
+	const message = {
+		to: process.env.SG_MAILER_TO,
+		from: process.env.SG_MAILER_FROM,
+		subject: 'You have received a new lead!',
+		text: 'this is where the text is',
+		html: `<p><strong>You got a new lead!</strong></p><br/><p><strong>Full name:</strong> ${event.queryStringParameters.name}</p><p><strong>E-mail address:</strong> ${event.queryStringParameters.email}</p><p><strong>Telephone number:</strong> ${event.queryStringParameters.telephone}</p><p><strong>Country:</strong> ${event.queryStringParameters.country}</p><p><strong>Comapny name:</strong> ${event.queryStringParameters.company}</p><p><strong>Team size:</strong> ${event.queryStringParameters.teamSize}</p>`,
+	};
+
 	try {
-		const subject = event.queryStringParameters.name || 'World';
+		Mailer.send(message);
 		return {
 			statusCode: 200,
-			body: JSON.stringify({ message: `Hello ${subject}`, event }),
+			body: JSON.stringify({
+				message: 'A sales representative from our team will be in touch soon.',
+				event,
+			}),
 			// // more keys you can return:
 			// headers: { "headerName": "headerValue", ... },
 			// isBase64Encoded: true,
 		};
 	} catch (error) {
-		return { statusCode: 500, body: error.toString() };
+		return { statusCode: 500, message: error.toString() };
 	}
 };
 
