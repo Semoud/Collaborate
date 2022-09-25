b71d1ac8cce6b473527fa97ef20941163a88031b
“Semoud” <semoud.o@gmail.com>
2022-09-25 21:17:52 +0100

    updated asset file path

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -5,7 +5,7 @@ function NotFound() {
 		<div className='py-7 md:py-14 lg:py-28'>
 			<div className='main-container grid grid-cols-1 items-center gap-10 lg:grid-cols-2 lg:gap-20'>
 				<div className='relative overflow-hidden pb-video lg:order-last lg:pb-square'>
-					<img className='absolute h-full w-full object-cover' src='./assets/hero.jpg' alt='team working in creative office' />
+					<img className='absolute h-full w-full object-cover' src='./../assets/hero.jpg' alt='team working in creative office' />
 				</div>
 				<div>
 					<h1 className='text-3xl font-semibold md:text-6xl'>404.</h1>
