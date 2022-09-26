32f605f4d70e72c9f8319563e00c5a7a58d5c00c
“Semoud” <semoud.o@gmail.com>
2022-09-26 08:44:22 +0100

    removed logging response to client

--- client/functions/submission/submission.js
+++ client/functions/submission/submission.js
@@ -12,33 +12,13 @@ const handler = async event => {
 		html: `<p><strong>You got a new lead!</strong></p><br/><p><strong>Full name:</strong> ${name}</p><p><strong>E-mail address:</strong> ${email}</p><p><strong>Telephone number:</strong> ${telephone}</p><p><strong>Country:</strong> ${country}</p><p><strong>Comapny name:</strong> ${company}</p><p><strong>Team size:</strong> ${teamSize}</p>`,
 	};
 
-	// Mailer.send(message)
-	// 	.then(() => {
-	// 		return {
-	// 			statusCode: 200,
-	// 			message: 'A sales representative from our team will be in touch soon.',
-	// 		};
-	// 	})
-	// 	.catch(error => {
-	// 		return {
-	// 			statusCode: 500,
-	// 			message: error.toString(),
-	// 		};
-	// 	});
-
 	try {
 		await Mailer.send(message);
 		return {
 			statusCode: 200,
 			body: JSON.stringify({
 				message: 'A sales representative from our team will be in touch soon.',
-				event,
-				jsonBody: { name, email, telephone, country, company, teamSize },
-				data: message,
 			}),
-			// // more keys you can return:
-			// headers: { "headerName": "headerValue", ... },
-			// isBase64Encoded: true,
 		};
 	} catch (error) {
 		return { statusCode: 500, message: error.toString() };
