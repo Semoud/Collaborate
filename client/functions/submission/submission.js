695ea349c30841fae4b17a64bd85b91f6893ed15
“Semoud” <semoud.o@gmail.com>
2022-09-26 08:31:07 +0100

    changed to .then

--- client/functions/submission/submission.js
+++ client/functions/submission/submission.js
@@ -9,27 +9,39 @@ const handler = async event => {
 		to: process.env.SG_MAILER_TO,
 		from: process.env.SG_MAILER_FROM,
 		subject: 'You have received a new lead!',
-		text: 'this is where the text is',
 		html: `<p><strong>You got a new lead!</strong></p><br/><p><strong>Full name:</strong> ${name}</p><p><strong>E-mail address:</strong> ${email}</p><p><strong>Telephone number:</strong> ${telephone}</p><p><strong>Country:</strong> ${country}</p><p><strong>Comapny name:</strong> ${company}</p><p><strong>Team size:</strong> ${teamSize}</p>`,
 	};
 
-	try {
-		Mailer.send(message);
-		return {
-			statusCode: 200,
-			body: JSON.stringify({
+	Mailer.send(message)
+		.then(() => {
+			return {
+				statusCode: 200,
 				message: 'A sales representative from our team will be in touch soon.',
-				event,
-				jsonBody: { name, email, telephone, country, company, teamSize },
-				data: message,
-			}),
-			// // more keys you can return:
-			// headers: { "headerName": "headerValue", ... },
-			// isBase64Encoded: true,
-		};
-	} catch (error) {
-		return { statusCode: 500, message: error.toString() };
-	}
+			};
+		})
+		.catch(error => {
+			return {
+				statusCode: 500,
+				message: error.toString(),
+			};
+		});
+
+	// try {
+	// 	return {
+	// 		statusCode: 200,
+	// 		body: JSON.stringify({
+	// 			message: 'A sales representative from our team will be in touch soon.',
+	// 			event,
+	// 			jsonBody: { name, email, telephone, country, company, teamSize },
+	// 			data: message,
+	// 		}),
+	// 		// // more keys you can return:
+	// 		// headers: { "headerName": "headerValue", ... },
+	// 		// isBase64Encoded: true,
+	// 	};
+	// } catch (error) {
+	// 	return { statusCode: 500, message: error.toString() };
+	// }
 };
 
 module.exports = { handler };
